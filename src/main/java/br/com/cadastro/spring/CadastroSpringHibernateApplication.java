6ae93aef18f000a617e10d376a18f031560468b7
Eduardo de Matos Almeida <eduardomatosalmeida@gmail.com>
2019-12-04 02:06:41 -0300

    Iniciando implementãção - Primeiro commit

---
+++ src/main/java/br/com/cadastro/spring/CadastroSpringHibernateApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastro.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroSpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroSpringHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cadastro/spring/CadastroSpringHibernateApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.cadastro.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CadastroSpringHibernateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
